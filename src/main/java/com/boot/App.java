6d1f52951c3d962a00c94879881d7ceab3008df7
VS <vijay.shyamasundar@infostretch.com>
2017-01-10 12:49:04 -0500

    should give some code smell

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by upgundecha on 25/03/16.
  */
 @SpringBootApplication
-public class App {
+    public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
